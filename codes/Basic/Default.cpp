5254d0746e8f93516cf169228270e707ea334b16
Ji Kuai <rk427454171@gmail.com>
2022-08-11 22:18:17 +0800

    add default code and vimrc

---
+++ codes/Basic/Default.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using uint = unsigned int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+const double EPS  = 1e-8;
+const int INF = 0x3F3F3F3F;
+const ll LINF = 4611686018427387903;
+const int MOD = 1e9+7;
+#define pb push_back
+#define F first
+#define S second
+#define mid ((LB+RB)/2)
+#define mkp make_pair
+#define iter(x) x.begin(),x.end()
+#define aiter(a,n) a,a+n
+#define REP(n) for (int ___=n > 0 ? n : 0;___--;)
+#define REP0(i,n) for (int i=0,___=n;i<___;++i)
+#define REP1(i,n) for (int i=1,___=n;i<=___;++i)
+#define MEM(e,val) memset (e,val,sizeof(e))
+
+signed main() { ios::sync_with_stdio(0); cin.tie(0);
+
+}
