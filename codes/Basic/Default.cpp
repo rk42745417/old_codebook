533aea2d536bb450a54bfe50a1847b5f729338cc
Ji Kuai <rk427454171@gmail.com>
2022-08-11 22:24:35 +0800

    move const declaraitons in default code

--- codes/Basic/Default.cpp
+++ codes/Basic/Default.cpp
@@ -11,10 +11,6 @@ using vi = vector<int>;
 using vl = vector<ll>;
 using vvi = vector<vector<int>>;
 using vvl = vector<vector<ll>>;
-const double EPS  = 1e-8;
-const int INF = 0x3F3F3F3F;
-const ll LINF = 4611686018427387903;
-const int MOD = 1e9+7;
 #define pb push_back
 #define F first
 #define S second
@@ -26,6 +22,10 @@ const int MOD = 1e9+7;
 #define REP0(i,n) for (int i=0,___=n;i<___;++i)
 #define REP1(i,n) for (int i=1,___=n;i<=___;++i)
 #define MEM(e,val) memset (e,val,sizeof(e))
+const double EPS  = 1e-8;
+const int INF = 0x3F3F3F3F;
+const ll LINF = 4611686018427387903;
+const int MOD = 1e9+7;
 
 signed main() { ios::sync_with_stdio(0); cin.tie(0);
 
