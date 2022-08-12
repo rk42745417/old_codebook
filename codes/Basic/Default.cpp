f247f5494647cbdf0963075c324d66e7a67f375d
Ji Kuai <rk427454171@gmail.com>
2022-08-13 00:03:54 +0800

    add maxn to default code

--- codes/Basic/Default.cpp
+++ codes/Basic/Default.cpp
@@ -26,6 +26,7 @@ const double EPS  = 1e-8;
 const int INF = 0x3F3F3F3F;
 const ll LINF = 4611686018427387903;
 const int MOD = 1e9+7;
+const int maxn = 1e5 + 25;
 
 signed main() { ios::sync_with_stdio(0); cin.tie(0);
 
