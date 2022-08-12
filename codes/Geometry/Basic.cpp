228f5ae09a9ea6431f71ff2c65f594324f898721
Ji Kuai <rk427454171@gmail.com>
2022-08-13 00:02:36 +0800

    add dinic, convex hull

---
+++ codes/Geometry/Basic.cpp
@@ -0,0 +1,16 @@
+using pt = pair<ll, ll>;
+using ptf = pair<ld, ld>;
+pt operator+(pt a, pt b)
+{ return pt {a.F + b.F, a.S + b.S}; }
+pt operator-(pt a, pt b)
+{ return pt {a.F - b.F, a.S - b.S}; }
+ptf to_ptf(pt p) { return ptf {p.F, p.S}; }
+int sign(ll x) { return (x > 0) - (x < 0); }
+ll dot(pt a, pt b) { return a.F * b.F + a.S * b.S; }
+ll cross(pt a, pt b) { return a.F * b.S - a.S * b.F; }
+ld abs2(ptf a) { return dot(a, a); }
+ld abs(ptf a) { return sqrtl(dot(a, a)); }
+int ori(pt a, pt b, pt c)
+{ return sign(cross(b - a, c - a)); }
+bool operator(pt a, pt b)
+{ return a.F != b.F ? a.F < b.F : a.S < b.S; }
---
+++ codes/Graph/Dinic.cpp
@@ -0,0 +1,61 @@
+template<typename T>
+struct dinic{
+    const T IN_INF = (is_same_v<T, int>) ? INF : LINF;
+    struct E{
+        int v; T c; int r;
+        E(int v, T c, int r):
+            v(v), c(c), r(r){}
+    };
+    vector<E> adj[maxn];
+    pair<int, int> is[maxn]; // counts of edges
+    void add_edge(int u, int v, T c, int i){
+        is[i] = {u, adj[u].size()};
+        adj[u].pb(E(v, c, (int) adj[v].size()));
+        adj[v].pb(E(u, 0, (int) adj[u].size() - 1));
+    }
+    int n, s, t;
+    void init(int nn, int ss, int tt){
+        n = nn, s = ss, t = tt;
+        for(int i = 0; i <= n; ++i)
+            adj[i].clear();
+    }
+    int le[maxn], it[maxn];
+    int bfs(){
+        fill(le, le + maxn, -1); le[s] = 0;
+        queue<int> q; q.push(s);
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            for(auto [v, c, r]: adj[u]){
+                if(c > 0 && le[v] == -1)
+                    le[v] = le[u] + 1, q.push(v);
+            }
+        }
+        return ~le[t];
+    }
+    int dfs(int u, int f){
+        if(u == t) return f;
+        for(int &i = it[u]; i < (int) adj[u].size(); ++i){
+            auto &[v, c, r] = adj[u][i];
+            if(c > 0 && le[v] == le[u] + 1){
+                int d = dfs(v, min(c, f));
+                if(d > 0){
+                    c -= d;
+                    adj[v][r].c += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
+    }
+    T flow(){
+        T ans = 0, d;
+        while(bfs()){
+            fill(it, it + maxn, 0);
+            while((d = dfs(s, IN_INF)) > 0) ans += d;
+        }
+        return ans;
+    }
+    T rest(int i) {
+        return adj[is[i].first][is[i].second].c;
+    }
+};
