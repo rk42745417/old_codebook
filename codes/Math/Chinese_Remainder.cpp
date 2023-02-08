a3cf696ff909e43900c60a4a2df09fb03cca6290
Ji Kuai <rk427454171@gmail.com>
2023-02-08 10:12:26 +0800

    Fix CRT

--- codes/Math/Chinese_Remainder.cpp
+++ codes/Math/Chinese_Remainder.cpp
@@ -1,8 +1,9 @@
+// x % m1 = x1, x % m2 = x2
 ll chre(ll x1, ll m1, ll x2, ll m2){
     ll g = __gcd(m1, m2);
     if ((x2 - x1) % g) return -1; //no solution
     m1 /= g; m2 /= g;
-    ll p = get<0>(extgcd(m1, m2));
+    ll p = get<1>(extgcd(m1, m2));
     ll lcm = m1 * m2 * g;
     ll res = p * (x2 - x1) * m1 + x1;
     //might overflow, be cautious
--- codes/Math/Extgcd.cpp
+++ codes/Math/Extgcd.cpp
@@ -4,4 +4,4 @@ tuple<T, T, T> extgcd(T a, T b) {
     if(!b) return make_tuple(a, 1, 0);
     auto [d, x, y] = extgcd(b, a % b);
     return make_tuple(d, y, x - (a / b) * y);
-} // not tested
+}
